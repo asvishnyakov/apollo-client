4778f2698ab22c3f16e0c5feb7310a18dba88982
Aleksandr Vishniakov <asvishnyakov@hotmail.com>
2024-02-04 05:17:40 +0200

    feat: make BatchHttpLink extendable

--- src/link/batch-http/batchHttpLink.ts
+++ src/link/batch-http/batchHttpLink.ts
@@ -25,6 +25,10 @@ export namespace BatchHttpLink {
     "batchMax" | "batchDebounce" | "batchInterval" | "batchKey"
   > &
     Omit<HttpOptions, "useGETForQueries">;
+
+  export type DefaultOptions = Required<
+    Pick<BatchHttpLink.Options, "uri" | "print" | "includeUnusedVariables">
+  >;
 }
 
 /**
@@ -37,220 +41,231 @@ export class BatchHttpLink extends ApolloLink {
   private batchMax: number;
   private batcher: ApolloLink;
 
+  protected fetchParams: BatchHttpLink.DefaultOptions & BatchHttpLink.Options;
+
   constructor(fetchParams?: BatchHttpLink.Options) {
     super();
 
-    let {
-      uri = "/graphql",
-      // use default global fetch if nothing is passed in
+    this.fetchParams =
+      {
+        uri: "/graphql",
+        print: defaultPrinter,
+        includeUnusedVariables: false,
+        ...fetchParams,
+      } || {};
+
+    const {
       fetch: fetcher,
-      print = defaultPrinter,
-      includeExtensions,
-      preserveHeaderCase,
       batchInterval,
       batchDebounce,
       batchMax,
       batchKey,
-      includeUnusedVariables = false,
-      ...requestOptions
-    } = fetchParams || ({} as BatchHttpLink.Options);
+    } = this.fetchParams;
 
     // dev warnings to ensure fetch is present
     checkFetcher(fetcher);
 
-    //fetcher is set here rather than the destructuring to ensure fetch is
-    //declared before referencing it. Reference in the destructuring would cause
-    //a ReferenceError
+    // use default global fetch if nothing is passed in
     if (!fetcher) {
-      fetcher = fetch;
+      this.fetchParams.fetch = fetch;
     }
-
-    const linkConfig = {
-      http: { includeExtensions, preserveHeaderCase },
-      options: requestOptions.fetchOptions,
-      credentials: requestOptions.credentials,
-      headers: requestOptions.headers,
-    };
 
     this.batchDebounce = batchDebounce;
     this.batchInterval = batchInterval || 10;
     this.batchMax = batchMax || 10;
 
-    const batchHandler = (operations: Operation[]) => {
-      const chosenURI = selectURI(operations[0], uri);
-
-      const context = operations[0].getContext();
-
-      const clientAwarenessHeaders: {
-        "apollographql-client-name"?: string;
-        "apollographql-client-version"?: string;
-      } = {};
-      if (context.clientAwareness) {
-        const { name, version } = context.clientAwareness;
-        if (name) {
-          clientAwarenessHeaders["apollographql-client-name"] = name;
-        }
-        if (version) {
-          clientAwarenessHeaders["apollographql-client-version"] = version;
-        }
-      }
-
-      const contextConfig = {
-        http: context.http,
-        options: context.fetchOptions,
-        credentials: context.credentials,
-        headers: { ...clientAwarenessHeaders, ...context.headers },
-      };
-
-      const queries = operations.map(({ query }) => {
-        if (hasDirectives(["client"], query)) {
-          return removeClientSetsFromDocument(query);
-        }
-
-        return query;
-      });
-
-      // If we have a query that returned `null` after removing client-only
-      // fields, it indicates a query that is using all client-only fields.
-      if (queries.some((query) => !query)) {
-        return fromError<FetchResult[]>(
-          new Error(
-            "BatchHttpLink: Trying to send a client-only query to the server. To send to the server, ensure a non-client field is added to the query or enable the `transformOptions.removeClientFields` option."
-          )
-        );
-      }
-
-      //uses fallback, link, and then context to build options
-      const optsAndBody = operations.map((operation, index) => {
-        const result = selectHttpOptionsAndBodyInternal(
-          { ...operation, query: queries[index]! },
-          print,
-          fallbackHttpConfig,
-          linkConfig,
-          contextConfig
-        );
-
-        if (result.body.variables && !includeUnusedVariables) {
-          result.body.variables = filterOperationVariables(
-            result.body.variables,
-            operation.query
-          );
-        }
-
-        return result;
-      });
-
-      const loadedBody = optsAndBody.map(({ body }) => body);
-      const options = optsAndBody[0].options;
-
-      // There's no spec for using GET with batches.
-      if (options.method === "GET") {
-        return fromError<FetchResult[]>(
-          new Error("apollo-link-batch-http does not support GET requests")
-        );
-      }
-
-      try {
-        (options as any).body = serializeFetchParameter(loadedBody, "Payload");
-      } catch (parseError) {
-        return fromError<FetchResult[]>(parseError);
-      }
-
-      let controller: AbortController | undefined;
-      if (!options.signal && typeof AbortController !== "undefined") {
-        controller = new AbortController();
-        options.signal = controller.signal;
-      }
-
-      return new Observable<FetchResult[]>((observer) => {
-        fetcher!(chosenURI, options)
-          .then((response) => {
-            // Make the raw response available in the context.
-            operations.forEach((operation) =>
-              operation.setContext({ response })
-            );
-            return response;
-          })
-          .then(parseAndCheckHttpResponse(operations))
-          .then((result) => {
-            controller = undefined;
-            // we have data and can send it to back up the link chain
-            observer.next(result);
-            observer.complete();
-            return result;
-          })
-          .catch((err) => {
-            controller = undefined;
-            // if it is a network error, BUT there is graphql result info
-            // fire the next observer before calling error
-            // this gives apollo-client (and react-apollo) the `graphqlErrors` and `networkErrors`
-            // to pass to UI
-            // this should only happen if we *also* have data as part of the response key per
-            // the spec
-            if (err.result && err.result.errors && err.result.data) {
-              // if we dont' call next, the UI can only show networkError because AC didn't
-              // get andy graphqlErrors
-              // this is graphql execution result info (i.e errors and possibly data)
-              // this is because there is no formal spec how errors should translate to
-              // http status codes. So an auth error (401) could have both data
-              // from a public field, errors from a private field, and a status of 401
-              // {
-              //  user { // this will have errors
-              //    firstName
-              //  }
-              //  products { // this is public so will have data
-              //    cost
-              //  }
-              // }
-              //
-              // the result of above *could* look like this:
-              // {
-              //   data: { products: [{ cost: "$10" }] },
-              //   errors: [{
-              //      message: 'your session has timed out',
-              //      path: []
-              //   }]
-              // }
-              // status code of above would be a 401
-              // in the UI you want to show data where you can, errors as data where you can
-              // and use correct http status codes
-              observer.next(err.result);
-            }
-
-            observer.error(err);
-          });
-
-        return () => {
-          // XXX support canceling this request
-          // https://developers.google.com/web/updates/2017/09/abortable-fetch
-          if (controller) controller.abort();
-        };
-      });
-    };
-
-    batchKey =
-      batchKey ||
-      ((operation: Operation) => {
-        const context = operation.getContext();
-
-        const contextConfig = {
-          http: context.http,
-          options: context.fetchOptions,
-          credentials: context.credentials,
-          headers: context.headers,
-        };
-
-        //may throw error if config not serializable
-        return selectURI(operation, uri) + JSON.stringify(contextConfig);
-      });
-
     this.batcher = new BatchLink({
       batchDebounce: this.batchDebounce,
       batchInterval: this.batchInterval,
       batchMax: this.batchMax,
-      batchKey,
-      batchHandler,
+      batchKey: batchKey || this.batchKey,
+      batchHandler: this.batchHandler,
     });
+  }
+
+  public batchHandler(operations: Operation[]) {
+    const {
+      uri,
+      fetch: fetcher,
+      print,
+      includeExtensions,
+      preserveHeaderCase,
+      includeUnusedVariables,
+      fetchOptions,
+      credentials,
+      headers,
+    } = this.fetchParams;
+
+    const linkConfig = {
+      http: { includeExtensions, preserveHeaderCase },
+      options: fetchOptions,
+      credentials,
+      headers,
+    };
+
+    const chosenURI = selectURI(operations[0], uri);
+
+    const context = operations[0].getContext();
+
+    const clientAwarenessHeaders: {
+      "apollographql-client-name"?: string;
+      "apollographql-client-version"?: string;
+    } = {};
+    if (context.clientAwareness) {
+      const { name, version } = context.clientAwareness;
+      if (name) {
+        clientAwarenessHeaders["apollographql-client-name"] = name;
+      }
+      if (version) {
+        clientAwarenessHeaders["apollographql-client-version"] = version;
+      }
+    }
+
+    const contextConfig = {
+      http: context.http,
+      options: context.fetchOptions,
+      credentials: context.credentials,
+      headers: { ...clientAwarenessHeaders, ...context.headers },
+    };
+
+    const queries = operations.map(({ query }) => {
+      if (hasDirectives(["client"], query)) {
+        return removeClientSetsFromDocument(query);
+      }
+
+      return query;
+    });
+
+    // If we have a query that returned `null` after removing client-only
+    // fields, it indicates a query that is using all client-only fields.
+    if (queries.some((query) => !query)) {
+      return fromError<FetchResult[]>(
+        new Error(
+          "BatchHttpLink: Trying to send a client-only query to the server. To send to the server, ensure a non-client field is added to the query or enable the `transformOptions.removeClientFields` option."
+        )
+      );
+    }
+
+    //uses fallback, link, and then context to build options
+    const optsAndBody = operations.map((operation, index) => {
+      const result = selectHttpOptionsAndBodyInternal(
+        { ...operation, query: queries[index]! },
+        print,
+        fallbackHttpConfig,
+        linkConfig,
+        contextConfig
+      );
+
+      if (result.body.variables && !includeUnusedVariables) {
+        result.body.variables = filterOperationVariables(
+          result.body.variables,
+          operation.query
+        );
+      }
+
+      return result;
+    });
+
+    const loadedBody = optsAndBody.map(({ body }) => body);
+    const options = optsAndBody[0].options;
+
+    // There's no spec for using GET with batches.
+    if (options.method === "GET") {
+      return fromError<FetchResult[]>(
+        new Error("apollo-link-batch-http does not support GET requests")
+      );
+    }
+
+    try {
+      (options as any).body = serializeFetchParameter(loadedBody, "Payload");
+    } catch (parseError) {
+      return fromError<FetchResult[]>(parseError);
+    }
+
+    let controller: AbortController | undefined;
+    if (!options.signal && typeof AbortController !== "undefined") {
+      controller = new AbortController();
+      options.signal = controller.signal;
+    }
+
+    return new Observable<FetchResult[]>((observer) => {
+      fetcher!(chosenURI, options)
+        .then((response) => {
+          // Make the raw response available in the context.
+          operations.forEach((operation) => operation.setContext({ response }));
+          return response;
+        })
+        .then(parseAndCheckHttpResponse(operations))
+        .then((result) => {
+          controller = undefined;
+          // we have data and can send it to back up the link chain
+          observer.next(result);
+          observer.complete();
+          return result;
+        })
+        .catch((err) => {
+          controller = undefined;
+          // if it is a network error, BUT there is graphql result info
+          // fire the next observer before calling error
+          // this gives apollo-client (and react-apollo) the `graphqlErrors` and `networkErrors`
+          // to pass to UI
+          // this should only happen if we *also* have data as part of the response key per
+          // the spec
+          if (err.result && err.result.errors && err.result.data) {
+            // if we dont' call next, the UI can only show networkError because AC didn't
+            // get andy graphqlErrors
+            // this is graphql execution result info (i.e errors and possibly data)
+            // this is because there is no formal spec how errors should translate to
+            // http status codes. So an auth error (401) could have both data
+            // from a public field, errors from a private field, and a status of 401
+            // {
+            //  user { // this will have errors
+            //    firstName
+            //  }
+            //  products { // this is public so will have data
+            //    cost
+            //  }
+            // }
+            //
+            // the result of above *could* look like this:
+            // {
+            //   data: { products: [{ cost: "$10" }] },
+            //   errors: [{
+            //      message: 'your session has timed out',
+            //      path: []
+            //   }]
+            // }
+            // status code of above would be a 401
+            // in the UI you want to show data where you can, errors as data where you can
+            // and use correct http status codes
+            observer.next(err.result);
+          }
+
+          observer.error(err);
+        });
+
+      return () => {
+        // XXX support canceling this request
+        // https://developers.google.com/web/updates/2017/09/abortable-fetch
+        if (controller) controller.abort();
+      };
+    });
+  }
+
+  public batchKey(operation: Operation): string {
+    const { uri } = this.fetchParams;
+
+    const context = operation.getContext();
+
+    const contextConfig = {
+      http: context.http,
+      options: context.fetchOptions,
+      credentials: context.credentials,
+      headers: context.headers,
+    };
+
+    //may throw error if config not serializable
+    return selectURI(operation, uri) + JSON.stringify(contextConfig);
   }
 
   public request(operation: Operation): Observable<FetchResult> | null {
